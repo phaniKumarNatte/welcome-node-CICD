866a421852faafe874fb5d7e6e411c50d0c1737f
phani117 <phani@vgyanitservices.com>
2026-01-29 17:13:29 +0530

    Added GitHub Actions auto deploy

--- lambda.js
+++ lambda.js
@@ -2,7 +2,7 @@ exports.handler = async (event) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: "Welcome to my Node.js Lambda app 🚀"
+      message: "Auto deployed project 🚀"
     }),
   };
 };
